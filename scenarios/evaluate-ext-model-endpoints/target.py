2e3ae419b0f9ce203cf0f9c7c9a1e1f961a95677
Waqas Javed <waqasjaved02@Waqass-MacBook-Pro-2023.local>
2024-07-23 19:43:33 -0700

    fix

--- scenarios/evaluate-ext-model-endpoints/target.py
+++ scenarios/evaluate-ext-model-endpoints/target.py
@@ -10,19 +10,23 @@ from promptflow.tracing import trace
 
 class TargetEndpointClass:
 
+    global _env
+
     def __init__(self, env):
-        self.env = env
+        _env = env
         # contructor
 
     class Response(TypedDict):
         question: str
         answer: str
 
-    def __call__(self, question: str, model_type: str) -> Response:
-    # def call_external_endpoints(question: str, model_type: str) -> Response:
+    @trace
+    def __call__(self, *, question: str, model_type: str, **kwargs) -> Response:
 
         if (model_type == "tiny_llama"): 
             output = self.call_tiny_llama_endpoint(question)
+        elif (model_type == "phi3_mini_serverless"):
+            output = self.call_phi3_mini_serverless_endpoint(question)    
         else:
             output = self.call_default_endpoint(question)
         
@@ -32,14 +36,13 @@ class TargetEndpointClass:
 
     def call_tiny_llama_endpoint(question: str) -> Response:
 
-        endpoint = env["tiny_llama"]["endpoint"]
-        key = env["tiny_llama"]["key"]
+        print(_env["tiny_llama"]["endpoint"])
+        print(_env["tiny_llama"]["key"])
+
+        endpoint = _env["tiny_llama"]["endpoint"]
+        key = _env["tiny_llama"]["key"]
 
         headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ key) }
-
-        print(endpoint)
-        print(key)
-        print(headers)
 
         def query(payload):
             print(payload)
@@ -59,16 +62,12 @@ class TargetEndpointClass:
         answer = output["choices"][0]["message"]["content"]
         return { "question" : question  , "answer" : answer }
 
-    def call_phi3_mini_endpoint(question: str) -> str:
+    def call_phi3_mini_serverless_endpoint(question: str) -> str:
 
-        endpoint = env["phi3_mini"]["endpoint"]
-        key = env["phi3_mini"]["key"]
+        endpoint = _env["phi3_mini"]["endpoint"]
+        key = _env["phi3_mini"]["key"]
 
         headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ key) }
-
-        print(endpoint)
-        print(key)
-        print(headers)
 
         def query(payload):
             print(payload)
