b74818c2689bb986a6d3a8e8adf37be75fdab07e
Sydney <sydneylister@microsoft.com>
2024-10-14 11:22:07 -0700

    disable pre-commit errors

--- scenarios/evaluate/evaluate_custom/blocklist.py
+++ scenarios/evaluate/evaluate_custom/blocklist.py
@@ -4,9 +4,9 @@
 
 
 class BlocklistEvaluator:
-    def __init__(self, blocklist) -> None:
+    def __init__(self, blocklist) -> None:  # noqa: ANN101, ANN001
         self._blocklist = blocklist
 
-    def __call__(self: "BlocklistEvaluator", *, response: str):
+    def __call__(self: "BlocklistEvaluator", *, response: str):  # noqa: ANN204
         score = any(word in response for word in self._blocklist)
         return {"score": score}
