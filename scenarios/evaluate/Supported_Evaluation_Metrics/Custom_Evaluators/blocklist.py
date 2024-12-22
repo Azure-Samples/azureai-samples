a417c4584403df36e6619f41f08f0ff78334045e
Eitan Sela <eitan.sela@gmail.com>
2024-12-22 12:49:38 +0200

    Improved quality of the notebook
    - `BlocklistEvaluator` wasn't detecting words in `blocklist`.
    - `blocklist` init parameter for `BlocklistEvaluator` was fixed to be an array of words.

--- scenarios/evaluate/Supported_Evaluation_Metrics/Custom_Evaluators/blocklist.py
+++ scenarios/evaluate/Supported_Evaluation_Metrics/Custom_Evaluators/blocklist.py
@@ -8,5 +8,5 @@ class BlocklistEvaluator:
         self._blocklist = blocklist
 
     def __call__(self: "BlocklistEvaluator", *, response: str):  # noqa: ANN204
-        score = any(word in response for word in self._blocklist)
+        score = any(word in self._blocklist for word in response.split())
         return {"score": score}
