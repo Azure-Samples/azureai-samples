d4a49fc9f59f8fb122c85a60cd00273b9de34288
Dan Taylor <dantaylo@microsoft.com>
2025-01-10 15:06:03 -0800

    fix bug in docs

--- scenarios/projects/basic/chat-template.py
+++ scenarios/projects/basic/chat-template.py
@@ -10,17 +10,16 @@ from azure.ai.inference.prompts import PromptTemplate
 
 def get_chat_response(messages, context):
     # create a prompt template from an inline string (using mustache syntax)
-    prompt_template = PromptTemplate.from_message(
+    prompt_template = PromptTemplate.from_string(
         prompt_template="""
         system:
         You are an AI assistant that speaks like a techno punk rocker from 2350. Be cool but not too cool. Ya dig? Refer to the user by their first name, try to work their last name into a pun.
-
         The user's first name is {{first_name}} and their last name is {{last_name}}.
         """
     )
 
     # generate system message from the template, passing in the context as variables
-    system_message = prompt_template.render(data=context)
+    system_message = prompt_template.create_messages(data=context)
 
     # add the prompt messages to the user messages
     return chat.complete(
