5818be82cc47a8fc70dd95ad3a1c66c09c75d67a
Vasanthik07 <v-kvasanthi@microsoft.com>
2025-01-30 13:03:14 +0530

    Ran the pre-commit before commit the changes

--- scenarios/Agents/samples/bing-search/bing-python.py
+++ scenarios/Agents/samples/bing-search/bing-python.py
@@ -26,21 +26,19 @@ USAGE:
 import os
 from azure.ai.projects import AIProjectClient
 from azure.identity import DefaultAzureCredential
-from azure.ai.projects.models import BingGroundingTool
+from azure.ai.projects import BingGroundingTool
 
 
 # Create an Azure AI Client from a connection string, copied from your AI Studio project.
 # At the moment, it should be in the format "<HostName>;<AzureSubscriptionId>;<ResourceGroup>;<HubName>"
 # Customer needs to login to Azure subscription via Azure CLI and set the environment variables
-
+# connection string should be copied from the project in AI Studio
 project_client = AIProjectClient.from_connection_string(
     credential=DefaultAzureCredential(),
     conn_str=os.environ["PROJECT_CONNECTION_STRING"],
 )
 
-bing_connection = project_client.connections.get(
-    connection_name=os.environ["CONNECTION_NAME"]
-)
+bing_connection = project_client.connections.get(connection_name=os.environ["CONNECTION_NAME"])
 conn_id = bing_connection.id
 
 print(conn_id)
@@ -55,7 +53,7 @@ with project_client:
         name="my-assistant",
         instructions="You are a helpful assistant",
         tools=bing.definitions,
-        headers={"x-ms-enable-preview": "true"}
+        headers={"x-ms-enable-preview": "true"},
     )
     print(f"Created agent, ID: {agent.id}")
 
