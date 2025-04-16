69ec631b851e0a24f7225fe7f23f5b05d84870d3
Linda Li <139801625+lindazqli@users.noreply.github.com>
2025-04-16 11:45:22 -0700

    Update filename in openapi_tripadvisor.py docstring

--- scenarios/docs-samples/python/agents/project-sdk/openapi_tripadvisor.py
+++ scenarios/docs-samples/python/agents/project-sdk/openapi_tripadvisor.py
@@ -22,7 +22,7 @@ from azure.identity import DefaultAzureCredential
 from azure.ai.projects.models import OpenApiTool, OpenApiConnectionAuthDetails, OpenApiConnectionSecurityScheme, OpenApiAnonymousAuthDetails
 
 
-# Create an Azure AI Client from a connection string, copied from your AI Studio project.
+# Create an Azure AI Client from a connection string, copied from your AI Foundry project.
 # At the moment, it should be in the format "<HostName>;<AzureSubscriptionId>;<ResourceGroup>;<HubName>"
 # Customer needs to login to Azure subscription via Azure CLI and set the environment variables
 
